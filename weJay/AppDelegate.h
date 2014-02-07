f759b6c5ede3d445e2715d47edeb9f3eadecb255
Brian Corbin <briancorbin@crockett-204.dynamic2.rpi.edu>
2014-02-07 17:25:19 -0500

    Initial Commit

---
+++ weJay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  weJay
+//
+//  Created by Brian Corbin on 2/7/14.
+//  Copyright (c) 2014 Brian Corbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
