aa3c3aa5a1d77a36d8b371bebd06aaa71a645454
Brian Corbin <brian.william.corbin@gmail.com>
2014-03-09 11:10:44 -0400

    Initial Commit

---
+++ weJay/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  weJay
+//
+//  Created by Brian Corbin on 3/9/14.
+//  Copyright (c) 2014 Brian Corbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
---
+++ weJay/MainMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainMenuViewController.m
+//  weJay
+//
+//  Created by Brian Corbin on 3/9/14.
+//  Copyright (c) 2014 Brian Corbin. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
