9ed0aaa4b7b2756107c0738a758ce35a86602e6f
Brian Corbin <brian.william.corbin@gmail.com>
2014-03-17 22:37:19 -0400

    Left and Right SW Reveals Working!

---
+++ weJay/RightSWRevealViewController.h
@@ -0,0 +1,13 @@
+//
+//  RightSWRevealViewController.h
+//  weJay
+//
+//  Created by Brian Corbin on 3/17/14.
+//  Copyright (c) 2014 Brian Corbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RightSWRevealViewController : UIViewController
+
+@end
